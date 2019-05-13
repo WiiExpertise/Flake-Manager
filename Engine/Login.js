b67ab43c1a85875986405f53c3d0a66a44c6c5ef
ro <44623254+rsakeys@users.noreply.github.com>
2019-05-13 17:33:19 +0100

    Update Login.js

--- Engine/Login.js
+++ Engine/Login.js
@@ -32,35 +32,24 @@ class Login{
     }
 
     async login(){
-        try{
-            this.username = this.request.body.username;
-            this.password = this.request.body.password;
-            let user = await this.database.penguin.findOne({where: {Username: this.username}});
-            if(user){
-                let password = user.Password.replace(/^\$2y(.+)$/i, '$2a$1');
-                if(!await this.checkRow(`Username`, this.username)){
-                    this.type = 'username_not_found';
-                    this.response.render('index', this.displaySite());
-                }
-    
-                else if(!await bcrypt.compare(await this.getHash(), password)){
-                    this.type = 'incorrect_password';
-                    this.response.render('index', this.displaySite());
-                }
-    
-                else{
-                    this.request.session.loggedin = true;
-                    this.request.session.username = this.username;
-                    this.response.redirect('/panel');
-                }
-            }
-            else{
-                this.type = 'username_not_found';
-                this.response.render('index', this.displaySite());
-            }
+        this.username = this.request.body.username;
+        this.password = this.request.body.password;
+        let user = await this.database.penguin.findOne({where: {Username: this.username}});
+        
+        if(!user){
+            this.type = 'username_not_found';
+            this.response.render('index', this.displaySite());
         }
-        catch(e){
-            console.log(e);
+    
+        else if(!await bcrypt.compare(await this.getHash(), user.Password.replace(/^\$2y(.+)$/i, '$2a$1'))){
+            this.type = 'incorrect_password';
+            this.response.render('index', this.displaySite());
+        }
+
+        else{
+            this.request.session.loggedin = true;
+            this.request.session.username = this.username;
+            this.response.redirect('/panel');
         }
     }
 
